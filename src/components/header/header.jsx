de55c5eb3dcef442c6051fe662eda8379aabba0a
bakytdev <azimbakyt111@gmail.com>
2024-02-05 18:53:14 +0600

    add scroll fun

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../style/header.css'
 import logo from '../../assets/img/logo-taxi.svg'
 const Header = () => {
+
+    const [scrool, setScroll] = useState(0);
+
+    window.addEventListener('scroll',() => setScroll(window.scrollY))
+
     return (
-        <header id="header">
+        <header id="header"
+                style={{background:scrool > 30 ? "#363636": "none",transition: '.4s'}}
+        >
             <div className="container">
                 <div className="header">
+
 
                     <img src={logo} alt="img"/>
 
                     <div className="header-nav">
-                        <a href="#">Главная</a>
-                        <a href="#">Услуги</a>
-                        <a href="#">О нас</a>
+                        <a href="#"
+                           style={{color: scrool > 0 && scrool < 570 ? '#FFC71A' : 'white'}}
+                        >Главная</a>
+                        <a href="#block"
+                           style={{color: scrool > 570 && scrool < 1718 ? '#FFC71A' : 'white'}}
+                        >Услуги</a>
+                        <a href="#about"
+                           style={{color: scrool > 1718 ? '#FFC71A' : 'white'}}
+                        >О нас</a>
                         <a href="#">Контакты</a>
-
+                        {/*<a href="">{scrool}</a>*/}
                     </div>
-
-
 
                 </div>
             </div>
